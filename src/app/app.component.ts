5dea18027d7ac2ac9813f8e80f299488a4b40095
TomaszGorczyca <164004387+TomaszGorczyca@users.noreply.github.com>
2025-02-25 23:24:58 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AComponent } from './a/a.component';
 import { BComponent } from './b/b.component'; 
+//imports child components to be used in this component
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',// Defines the custom HTML tag <app-root> as the root component
   imports: [RouterOutlet, AComponent,BComponent],
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',// Path to the component’s HTML template
   styleUrl: './app.component.css'
 })
 export class AppComponent {
