8a55eb3b6bd58a945c7676b98cd7ed2204e9d15a
TomaszGorczyca <164004387+TomaszGorczyca@users.noreply.github.com>
2025-02-25 23:17:39 +0000

    Update a.component.spec.ts

--- src/app/a/a.component.spec.ts
+++ src/app/a/a.component.spec.ts
@@ -1,9 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';//import testing
 
 import { AComponent } from './a.component';
 
 describe('AComponent', () => {
-  let component: AComponent;
+  let component: AComponent;// veriables to hold the component instance
   let fixture: ComponentFixture<AComponent>;
 
   beforeEach(async () => {
@@ -16,7 +16,7 @@ describe('AComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+//test case cheacks if the component is crated succesfully
   it('should create', () => {
     expect(component).toBeTruthy();
   });
